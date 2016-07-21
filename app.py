4fd5edadc0ea7bc0976d46398b1cfbf874441118
rishabhcsinha <rishabh.csinha@gmail.com>
2016-07-22 02:08:45 +0530

    test1

--- app.py
+++ app.py
@@ -7,9 +7,20 @@ app.config["SECRET_KEY"] = 'a|\x92_,\xc5\xb3\x1ez\x11\xd4=RwK\x92;\xa6f\xbc\xcfD
 # import os
 # basedir = os.path.abspath(os.path.dirname(__file__))
 
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file_list=['test1','test2']
+        if request.form['file'] in file_list:
+            return 'sahi hai boss'
+        else :
+            flash ('galat value daala hai')
+            return redirect(url_for('index'))
 
+    else :
+    	return render_template('index.html')
 
-from views import *
+# from views import *
  
 if __name__ == '__main__':
 	app.run()
